c7e1500612ebb17088b14c90e7ce0d58b51d1363
930112jaywang-art <930112jaywang@gmail.com>
2026-09-23 14:28:40 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,27 @@
+// 離線用：App 本身的檔案「先上網抓最新版，失敗才用快取」；字型「先用快取」
+const CACHE = 'topik-v1';
+const FILES = ['./', './index.html', './data.js', './manifest.webmanifest', './icon-180.png', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+
+self.addEventListener('fetch', e => {
+  const req = e.request;
+  if (req.method !== 'GET') return;
+  const url = new URL(req.url);
+  if (url.hostname === 'fonts.googleapis.com' || url.hostname === 'fonts.gstatic.com') {
+    e.respondWith(caches.match(req).then(hit => hit || fetch(req).then(res => {
+      const copy = res.clone(); caches.open(CACHE).then(c => c.put(req, copy)); return res;
+    })));
+    return;
+  }
+  if (url.origin !== location.origin) return;
+  e.respondWith(fetch(req).then(res => {
+    const copy = res.clone(); caches.open(CACHE).then(c => c.put(req, copy)); return res;
+  }).catch(() => caches.match(req, {ignoreSearch: true}).then(hit => hit || caches.match('./index.html'))));
+});
